7b1597261464f036fdfb7403b331e24e10a9e5dd
Cohen Coberly <22184868+polycont@users.noreply.github.com>
2022-08-03 21:02:23 -0500

    Complete RPS project

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 /*
 
+THE PROBLEM:
+
 I need to create an implementation of Rock Paper Scissors that can be played 
 entirely from the console. It does not have an interface, but it will need
 to be able to take text inputs from the player -- three, to be specific:
@@ -10,19 +12,15 @@ The program must keep track of the score for each 'player,' and continue initiat
 until one player earns five points. When that happens, the program needs to stop the game entirely
 and announce the winner.
 
-The flow will look something like this.
+*/
 
-Create a function to compare the two scores. (?)
+/* TODOS
 
-Greet the player by name and instruct them to enter rock, paper, or scissors.
-Ensure the player's input does not need to be case-sensitive.
-Create function to get the computer's selection.
-
-Compare the player's selection against the computer's selection.
-
-Wrap the following in a loop that stops when either player's score reaches 5.
+TODO: Create a play again function?
+TODO: Add while loop to handle invalid player input
 
 */
+
 
 /* Prompt the player for their name and store it in a variable. */
 const playerName = prompt('What is your name?');
@@ -32,104 +30,73 @@ Create a variable to track the computer's score with an initial value of 0. */
 let playerScore = 0;
 let computerScore = 0;
 
-
-
-
-
-let computerChoice;
-
-/* Greet the player by name and instruct them to enter rock, paper, or scissors. */
-function getPlayerChoice() {
-    let playerChoice = prompt(`Hello, ${playerName}. Welcome to Rock Paper Scissors. Please enter your choice!`).toLowerCase();
-    return playerChoice;
-}
-
-/* Create a function to calculate the computer's choice and return the value */
-function getComputerChoice() {
-    let choiceCalc = Math.floor(Math.random() * 100);
-    if (choiceCalc > 33 && choiceCalc < 66) {
-        return 'rock';
-    } else if (choiceCalc < 33) {
-        return 'paper';
-    } else if (choiceCalc > 66) {
-        return 'scissors';
-    }
-}
-
 /* Create a function to play the game */
-function playRPS(getPlayerChoice, getComputerChoice) {
+function playRPS() {
+
+    /* Instruct them to enter rock, paper, or scissors. */
+    function getPlayerChoice() {
+        let playerChoice = prompt('Please enter your choice!').toLowerCase();
+        console.log(playerChoice);
+        return playerChoice;
+    }
+
+    /* Create a function to calculate the computer's choice and return the value */
+    function getComputerChoice() {
+        let choiceCalc = Math.floor(Math.random() * 100);
+        if (choiceCalc > 33 && choiceCalc < 66) {
+            return 'rock';
+        } else if (choiceCalc < 33) {
+            return 'paper';
+        } else if (choiceCalc > 66) {
+            return 'scissors';
+        }
+    }
+
+    alert(`Hello, ${playerName}. Welcome to Rock Paper Scissors!`)
+
     /* Create a loop to continue playing the game until the score reaches 5 */
     while (playerScore < 5 && computerScore < 5) {
-        if (getPlayerChoice() === 'rock' && getComputerChoice() === 'paper') {
+        
+        /* Declare variables for player & computer choices */
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+
+        /* Compare player's choice to computer's and award a point (or a tie) based on which selection wins */
+        if (playerSelection === 'rock' && computerSelection === 'paper') {
             computerScore++;
             console.log('Sorry, you lost this round! Paper beats Rock.');
-        } else if (getPlayerChoice() === 'rock' && getComputerChoice() === 'scissors') {
+        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
             playerScore++
             console.log('Nice! You won this round! Rock beats Scissors.');
-        } else if (getPlayerChoice() === 'rock' && getComputerChoice() === 'rock') {
+        } else if (playerSelection === 'rock' && computerSelection === 'rock') {
             console.log('This round is a tie! No points awarded this time.');
-        }
-
-        if (getPlayerChoice() === 'paper' && getComputerChoice() === 'paper') {
+        } else if (playerSelection === 'paper' && computerSelection === 'paper') {
             console.log('This round is a tie! No points awarded this time.');
-        } else if (getPlayerChoice() === 'paper' && getComputerChoice() === 'scissors') {
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
             computerScore++;
             console.log('Sorry, you lost this round! Scissors beat Paper.');
-        } else if (getPlayerChoice() === 'paper' && getComputerChoice() === 'rock')
+        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
             playerScore++;
             console.log('Nice! You won this round! Paper beats Rock.');
-
-        if (getPlayerChoice() === 'scissors' && getComputerChoice() === 'paper') {
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
             playerScore++
             console.log('Nice! You won this round! Scissors beat Paper.');
-        } else if (getPlayerChoice() === 'scissors' && getComputerChoice() === 'scissors') {
+        } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
             console.log('This round is a tie! No points awarded this time.');
-        } else if (getPlayerChoice() === 'scissors' && getComputerChoice() === 'rock') {
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
             computerScore++
             console.log('Sorry, you lost this round! Paper beats Rock.');
         }
             
 }
 
+/* Print a win or loss message to the console. */
+if (playerScore === 5) {
+    console.log('Congratulations! You won!');
+} else {
+    console.log('Darn! Looks like the computer wins this time.');
 }
 
-playRPS(getPlayerChoice(), getComputerChoice());
+}
 
-/*
-
-IF player selected ROCK and the computer selected PAPER...
-    Increase computer's score by 1.
-    Log 'Sorry, you lost this round! Paper beats Rock.' to the console.
-ELSE IF player selected ROCK and the computer selected SCISSORS...
-    Increase player's score by 1.
-    Log 'Nice! You won this round! Rock beats Scissors.' to the console.
-ELSE IF player selected ROCK and the computer selected ROCK...
-    Log 'This round is a tie! No points awarded this time.' to the console.
-
-ELSE IF player selected PAPER and the computer selected PAPER...
-    Log 'This round is a tie! No points awarded this time.' to the console.
-ELSE IF player selected PAPER and the computer selected SCISSORS...
-    Increase computer's score by 1.
-    Log 'Sorry, you lose this round! Paper beats Rock.' to the console.
-ELSE IF player selected PAPER and the computer selected ROCK...
-    Increase player's score by 1.
-    Log 'Nice! You won this round! Rock beats Scissors.' to the console.
-
-ELSE IF player selected SCISSORS and the computer selected PAPER...
-    Increase player's score by 1.
-    Log 'Nice! You won this round! Scissors beat Paper.' to the console.
-ELSE IF player selected SCISSORS and the computer selected SCISSORS...
-    Log 'This round is a tie! No points awarded this time.' to the console.
-ELSE IF player selected SCISSORS and the computer selected ROCK...
-    Increase computer's score by 1.
-    Log 'Sorry, you lose this round! Rock beats scissors.' to the console.
-
-Exit loop
-Log a congratulations message to the console. 
-
-TODO: Create a play again function?
-TODO: Add while loop to handle invalid player input
-
-
-
-*/
+playRPS();
